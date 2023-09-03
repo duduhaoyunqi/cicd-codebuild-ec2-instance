cac541d72ba0eee20f7489fac6da2a62d2e14eab
duduhaoyunqi <diaoke901214@gmail.com>
2023-09-04 01:33:48 +0800

    modified

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,12 @@
 # create ec2 instance
-#
-#
-#
-#
+
+provider "aws" {
+  region = "cn-northwest-1"
+  profile = "codebuild-user"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-01cb2ecea35798f3f"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0f9cb66c7400d1b68"
+}
