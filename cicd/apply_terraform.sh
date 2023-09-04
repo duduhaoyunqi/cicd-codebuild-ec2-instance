a3a0d27242006dc8caf94229cf20ec81e9781280
duduhaoyunqi <diaoke901214@gmail.com>
2023-09-04 13:17:25 +0800

    modified

---
+++ cicd/apply_terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set eu
+
+cd ..
+
+terraform init
+
+terraform apply -auto-approve
+
+# terraform destroy -auto-approve
--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,16 @@ provider "aws" {
   profile = "codebuild-user"
 }
 
+# store the terraform state file in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key = "build/terraform.tfstate"
+    region = "cn-northwest-1"
+    profile = "codebuild-user"
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-01cb2ecea35798f3f"
   instance_type = "t2.micro"
