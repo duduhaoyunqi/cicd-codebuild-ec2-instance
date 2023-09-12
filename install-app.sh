c2c6478a8393282b8256beaf56e556bd77e53020
duduhaoyunqi <diaoke901214@gmail.com>
2023-09-12 20:33:39 +0800

    modified

--- install-app.sh
+++ install-app.sh
@@ -4,9 +4,9 @@ yum update -y
 yum install -y httpd wget
 sed -i '/^Listen 80/cListen 9999' /etc/httpd/conf/httpd.conf
 cd /var/www/html
-wget https://github.com/azeezsalu/jupiter/archive/refs/heads/main.zip
+wget https://github.com/azeezsalu/techmax/archive/refs/heads/main.zip
 unzip main.zip
-cp -r jupiter-main/* /var/www/html/
-rm -rf jupiter-main main.zip
-systemctl enable httpd
+cp -r techmax-main/* /var/www/html/
+rm -rf techmax-main main.zip
+systemctl enable httpd 
 systemctl start httpd
