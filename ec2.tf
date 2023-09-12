480e2dd4c1f170e8ca312026489812a48e7f849a
duduhaoyunqi <diaoke901214@gmail.com>
2023-09-12 15:40:20 +0800

    modified

--- ec2.tf
+++ ec2.tf
@@ -19,4 +19,5 @@ resource "aws_instance" "app_server" {
   ami           = "ami-01cb2ecea35798f3f"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0f9cb66c7400d1b68"
+  user_data = file("install-app.sh")
 }
