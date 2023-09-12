28e014b7a4894561e3bbeb50c91f2be4b05fc428
duduhaoyunqi <diaoke901214@gmail.com>
2023-09-12 23:32:54 +0800

    modified

--- ec2.tf
+++ ec2.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "ec2_security_group" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  
+   ingress {
+    description      = "http access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port        = 0
